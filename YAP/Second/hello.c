06e2565c10c57029a23d7264071cbd034601718a
red <62202628+red23714@users.noreply.github.com>
2025-11-13 19:40:14 +0300

    created 728 byte hello world

--- YAP/Second/hello.c
+++ YAP/Second/hello.c
@@ -1,7 +1,12 @@
-extern int printf (const char* format, ...);
+/* Минимальный Hello World на C без библиотек */
+typedef unsigned long size_t;
 
-int main()
-{
-  printf("Hello, world!\n");
+/* объявляем syscall, реализованный в sys_write.S */
+extern long sys_write(int fd, const void *buf, size_t count);
+
+/* точка входа (вместо main) */
+int kmain(void) {
+  const char msg[] = "hello\n";
+  sys_write(1, msg, sizeof(msg) - 1);
   return 0;
 }
