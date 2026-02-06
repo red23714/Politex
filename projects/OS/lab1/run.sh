ae64f3413345c3733213935cc44e48829f06c0a6
red <redredovich0@gmail.com>
2026-02-07 02:56:57 +0300

    Make coolest Cum OS ever

--- projects/OS/lab1/run.sh
+++ projects/OS/lab1/run.sh
@@ -1,8 +1,17 @@
-rm bootsect.bin kernel.bin
-# fasm bootsect.asm
-fasm test.asm
-fasm kernel.asm
-# qemu-system-i386 -fda bootsect.bin -fdb kernel.bin
+# Компилируем загрузчик
+fasm test.asm test.bin
 
-cat test.bin kernel.bin > os.bin
-qemu-system-i386 -fda os.bin
+# Компилируем ядро
+fasm test_kernel.asm kernel.bin
+
+# Создаем образ дискеты 1.44МБ (2880 секторов по 512 байт)
+dd if=/dev/zero of=disk.img bs=512 count=2880
+
+# Копируем загрузочный сектор
+dd if=test.bin of=disk.img conv=notrunc
+
+# Копируем ядро начиная со второго сектора
+dd if=kernel.bin of=disk.img bs=512 seek=1 conv=notrunc
+
+# Запускаем QEMU
+qemu-system-i386 -fda disk.img
--- projects/OS/lab1/test.asm
+++ projects/OS/lab1/test.asm
@@ -1,108 +1,104 @@
 use16
 org 0x7C00
 
-macro print let
-{
-    mov al, let
-    mov ah, 0x0e
-    int 0x10
-}
-
 start:
+    ; Инициализация сегментов
     xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7C00
-
-    ; Отладка
-    print 'S'
-    print ' '
     
-    ; ЗАГРУЗКА ЯДРА С ДИСКА A: (СЕКТОР 1!)
-    print 'R'
-    print ' '
+    ; Вывод сообщения
+    mov si, msg
+    call print_string
     
-    mov ah, 0x02
-    mov al, 1           ; 1 сектор
+    ; Загрузка ядра (сектор 2 и далее)
+    mov ah, 0x02        ; функция чтения
+    mov al, 4           ; читаем 4 сектора (2КБ для ядра)
     mov ch, 0           ; цилиндр 0
-    mov cl, 1           ; СЕКТОР 1 (важно!)
+    mov cl, 2           ; сектор 2 (сразу после загрузочного)
     mov dh, 0           ; головка 0
-    mov dl, 0x00        ; диск A:
+    mov dl, 0           ; дискета A:
     
-    mov ax, 0x1000      ; сегмент 0x1000
-    mov es, ax
-    xor bx, bx          ; смещение 0
+    ; Куда загружать (сразу после загрузчика)
+    mov bx, 0x1000
     
     int 0x13
     jc disk_error
     
-    print 'O'           ; OK
-    print 'K'
-    print ' '
+    ; Переход к загруженному ядру
+    mov si, load_ok_msg
+    call print_string
     
     jmp turn_protected
 
-disk_error:
-    print 'E'
-    print ' '
-    mov al, ah          ; код ошибки
-    call print_hex
-    jmp $
-
-print_hex:
-    push ax
-    shr al, 4
-    call .nibble
-    pop ax
-    and al, 0x0F
-    call .nibble
-    ret
-.nibble:
-    cmp al, 10
-    jl .digit
-    add al, 7
-.digit:
-    add al, '0'
-    mov ah, 0x0e
-    int 0x10
-    ret
-
 gdt:
+    ; Нулевой дескриптор
     db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-    db 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x9A, 0xCF, 0x00
-    db 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x92, 0xCF, 0x00
-gdt_info:
-    dw gdt_info - gdt - 1
-    dd gdt
+    ; Сегмент кода: base=0, size=4Gb, P=1, DPL=0, S=1(user),
+    ; Type=1(code), Access=00A, G=1, B=32bit
+    db 0xff, 0xff, 0x00, 0x00, 0x00, 0x9A, 0xCF, 0x00
+    ; Сегмент данных: base=0, size=4Gb, P=1, DPL=0, S=1(user),
+    ; Type=0(data), Access=0W0, G=1, B=32bit
+    db 0xff, 0xff, 0x00, 0x00, 0x00, 0x92, 0xCF, 0x00
+gdt_info: ; Данные о таблице GDT (размер, положение в памяти)
+    dw gdt_info - gdt - 1 ; Размер таблицы (2 байта)
+    dw gdt, 0 ; 32-битный физический адрес таблицы.
 
 turn_protected:
-    print 'P'
-    print ' '
-    
+    ; Отключение прерываний
     cli
-    lgdt [gdt_info]
-    
+
+    ; Загрузка размера и адреса таблицы дескрипторов
+    lgdt [gdt_info] ; Для GNU assembler должно быть "lgdt gdt_info"
+
+     ; Включение адресной линии А20
     in al, 0x92
     or al, 2
     out 0x92, al
-    
+ 
+    ; Установка бита PE регистра CR0 - процессор перейдет в защищенный режим
     mov eax, cr0
     or al, 1
     mov cr0, eax
-    
-    jmp 0x8:protected_mode
+
+    jmp 0x8:protected_mode ; "Дальний" переход для загрузки корректной информации в cs (архитектурные особенности не позволяют этого сделать напрямую).
 
 use32
 protected_mode:
-    mov ax, 0x10
-    mov ds, ax
+    ; Загрузка селекторов сегментов для стека и данных в регистры
+    mov ax, 0x10 ; Используется дескриптор с номером 2 в GDT
     mov es, ax
+    mov ds, ax
     mov ss, ax
     mov esp, 0x90000
+     
+    ; Передача управления загруженному ядру
+    call 0x1000 ; Адрес равен адресу загрузки в случае если ядро скомпилировано в "плоский" код
+
+    jmp $
     
-    call 0x10000
+disk_error:
+    mov si, error_msg
+    call print_string
     jmp $
 
+print_string:
+    mov ah, 0x0E
+.next_char:
+    lodsb
+    test al, al
+    jz .done
+    int 0x10
+    jmp .next_char
+.done:
+    ret
+
+msg db "Bootloader starting...", 13, 10, 0
+error_msg db "Disk error!", 13, 10, 0
+load_ok_msg db "Kernel loaded!", 13, 10, 0
+
+; Заполнение до 510 байт
 times 510 - ($ - $$) db 0
-db 0x55, 0xAA
+dw 0xAA55
