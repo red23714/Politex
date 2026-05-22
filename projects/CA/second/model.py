c9aa010b0075b55fdbab613fa04c40ee8ad21ae2
red <redredovich0@gmail.com>
2026-05-22 05:05:00 +0300

    CA make labs

--- projects/CA/second/model.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-import torchvision.models as models
-
-
-def get_model():
-    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-    model.fc = nn.Linear(model.fc.in_features, 2)
-    return model
